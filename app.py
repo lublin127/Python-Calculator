efe31de0d70672a023bc8774fae7e22e48704923
lublin127 <elon@marketer.co.il>
2023-11-24 11:35:02 +0200

    Adding Divider

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ n1 = int(input("Enter First Number: "))
 n2 = int(input("Enter Second Number: "))
 
 sum = n1+n2
-multiply = n1*n2
+mul = n1*n2
+div = n1/n2
 print(f"{n1}+{n2} = {sum}")
-print(f"{n1}*{n2} = {multiply}")
+print(f"{n1}*{n2} = {mul}")
+print(f"{n1}/{n2} = {div}")
+
+
