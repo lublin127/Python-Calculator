b90598f548de7b363791701147a714829e194a59
lublin127 <elon@marketer.co.il>
2023-11-24 11:18:55 +0200

    multipication

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-n1 = int(input("Enter First Number"))
-n2 = int(input("Enter Second Number"))
+n1 = int(input("Enter First Number: "))
+n2 = int(input("Enter Second Number: "))
 
 sum = n1+n2
+multiply = n1*n2
 print(f"{n1}+{n2} = {sum}")
+print(f"{n1}*{n2} = {multiply}")
